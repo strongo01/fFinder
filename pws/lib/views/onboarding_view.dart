d3fd95a889e7d38bca33e25d0ba26fe5dc29e2f9
Strongo01 <maxveldhuis@outlook.com>
2025-11-25 13:11:54 +0100

    Snackbar gestyled

--- pws/lib/views/onboarding_view.dart
+++ pws/lib/views/onboarding_view.dart
@@ -176,9 +176,10 @@ class _OnboardingViewState extends State<OnboardingView> {
   void _showError(String message) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        content: Text(message),
+        content: Text(message, style: const TextStyle(color: Colors.white)),
         backgroundColor: Theme.of(context).colorScheme.error,
         behavior: SnackBarBehavior.floating, //snackbar zwevend maken
+        
       ),
     );
   }
