60f8f8df19252b0fad1e181dbecf44d7e284f809
strongo01 <maxveldhuis@outlook.com>
2025-12-13 00:09:10 +0100

    Set fixed version code and version name in build.gradle.kts

--- pws/android/app/build.gradle.kts
+++ pws/android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode = 4
+        versionName = "1.0.0"
     }
 
     signingConfigs {
