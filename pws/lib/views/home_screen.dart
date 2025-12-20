81f1c2df376c30de5ecf68df104a96f813683370
strongo01 <maxveldhuis@outlook.com>
2025-12-20 16:33:52 +0100

    Bump versionCode to 10 and update app version to 1.0.5

--- pws/lib/views/home_screen.dart
+++ pws/lib/views/home_screen.dart
@@ -63,7 +63,7 @@ class _HomeScreenState extends State<HomeScreen> {
   final GlobalKey _settingsKey = GlobalKey();
   final GlobalKey _weightKey = GlobalKey();
 
-  static const String _appVersion = '1.0.4';
+  static const String _appVersion = '1.0.5';
 
   late TutorialCoachMark tutorialCoachMark;
 
